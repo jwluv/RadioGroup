1a4778a7fb27130185559507633f3c0ec660b653
EDU <jwluv1220@gmail.com>
2018-10-26 11:46:37 +0900

    Initial commit

---
+++ app/src/main/java/com/lyna/www/radiogroup/MainActivity.java
@@ -0,0 +1,28 @@
+package com.lyna.www.radiogroup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    RadioGroup radioGroup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                String text="CheckedId : " + checkedId;
+                Toast.makeText(getApplicationContext(),text,Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
